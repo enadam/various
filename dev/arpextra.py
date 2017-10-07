c00b2665847d06cc774c5f33c9e913331bcba019
Adam Endrodi <adam@ginf.hu>
2017-10-07 13:19:38 +0200

    dev/arpextra: new code drop
    More parameter types, improvements and bug fixes.

--- dev/arpextra.py
+++ dev/arpextra.py
@@ -2,9 +2,34 @@
 import argparse
 import re, socket
 
+REVISION = "$Id: 98eb427eb996f3b615815ad626f718371b672eb6 $"
+
+# Change some user-visible texts.
+argparse_gettext = argparse._
+def override_gettext(message):
+	if message == "usage: ":
+		message = message.capitalize()
+	elif message == "optional arguments":
+		message = "Options"
+	elif message == "positional arguments":
+		message = "Operands"
+	elif message == "show this help message and exit":
+		message = "Show this help."
+	return argparse_gettext(message)
+argparse._ = override_gettext
+
 __all__ = [ "ArgumentParserExtra" ]
 class ArgumentParserExtra(argparse.ArgumentParser):
-	class MandatorySubCommands(argparse._SubParsersAction):
+	ArgumentTypeError = argparse.ArgumentTypeError
+
+	class SubParsers(argparse._SubParsersAction):
+		def add_parser(self, *args, **kwargs):
+			# make the subcommand's 'description' the same as
+			# its help (in the parent) unless specified otherwise
+			if "help" in kwargs and "description" not in kwargs:
+				kwargs["description"] = kwargs["help"]
+			return super().add_parser(*args, **kwargs)
+	class MandatorySubCommands(SubParsers):
 		def __call__(self, parser, ns, values, *args, **kwargs):
 			value = values[0]
 			alternatives = self.choices.keys()
@@ -102,8 +127,19 @@ class ArgumentParserExtra(argparse.ArgumentParser):
 		def __init__(self, *args, **kwargs):
 			super().__init__(*args, **kwargs)
 			self.nargs = 0
-			self.option_strings.append(
-				"--no-" + self.option_strings[0][2:])
+
+			if not self.option_strings:
+				raise TypeError("positional argument "
+						"cannot be on-off")
+
+			long_opts = [ opt for opt in self.option_strings
+					if opt.startswith("--") ]
+			if not long_opts:
+				raise TypeError("on-off type argument "
+						"requires a long option name")
+
+			for opt in long_opts:
+				self.option_strings.append("--no-" + opt[2:])
 		def __call__(self, parser, namespace, values, option_string,
 				*args, **kwargs):
 			setattr(namespace, self.dest,
@@ -118,7 +154,9 @@ class ArgumentParserExtra(argparse.ArgumentParser):
 			self.nargs = '?'
 			self.metavar = "on|off"
 		def __call__(self, parser, namespace, values, *args, **kwargs):
-			if values is None or values.lower() in self.on:
+			if self.default is not None and values is self.default:
+				return
+			elif values is None or values.lower() in self.on:
 				setattr(namespace, self.dest, True)
 			elif values.lower() in self.off:
 				setattr(namespace, self.dest, False)
@@ -126,32 +164,93 @@ class ArgumentParserExtra(argparse.ArgumentParser):
 				raise argparse.ArgumentError(self,
 					"boolean value expected")
 
-	def __init__(self, *args, **kwargs):
+	class Password(argparse.Action):
+		def __init__(self, *args, **kwargs):
+			super().__init__(*args, **kwargs)
+			self.nargs = 0
+		def __call__(self, parser, namespace, values, *args, **kwargs):
+			import sys
+
+			if not sys.stdin.isatty():
+				sys.stdout.write("Password: ")
+				sys.stdout.flush()
+				password = sys.stdin.readline().rstrip('\n')
+				print()
+				setattr(namespace, self.dest, password)
+				return
+
+			import getpass
+
+			try:
+				password = getpass.getpass()
+				pwdagain = getpass.getpass("Password again: ")
+			except EOFError:
+				raise argparse.ArgumentError(self,
+					"interrupted")
+
+			if password != pwdagain:
+				raise argparse.ArgumentError(self,
+					"passwords do not match")
+			setattr(namespace, self.dest, password)
+
+	class MyHelpFormatter(argparse.HelpFormatter):
+		def add_text(self, text):
+			if not isinstance(text, (tuple, list)):
+				super().add_text(text)
+				return
+			for block in text:
+				super().add_text(block)
+
+		def _format_args(self, action, default_metavar):
+			get_metavar = self._metavar_formatter(action,
+					default_metavar)
+			if action.nargs == argparse.ZERO_OR_MORE:
+				return "[%s] ..." % get_metavar(1)
+			elif action.nargs == argparse.ONE_OR_MORE:
+				return "%s ..." % get_metavar(1)
+			else:
+				return super()._format_args(action,
+							default_metavar)
+
+	def __init__(self, *args, add_help=argparse.SUPPRESS,
+			formatter_class=MyHelpFormatter, **kwargs):
+		kwargs["add_help"] = add_help is True
+		kwargs["formatter_class"] = formatter_class
 		if "conflict_handler" not in kwargs:
 			kwargs["conflict_handler"] = "resolve"
+
 		super().__init__(*args, **kwargs)
 		self.some_of_groups = [ ]
 		self.register("action", "on-off",	self.OnOff)
 		self.register("action", "boolean",	self.Boolean)
+		self.register("action", "password",	self.Password)
 		self.register("action", "store_true",	self.StoreTrue)
 		self.register("action", "append",	self.AppendAction)
+		self.register("action", "parsers",	self.SubParsers)
+
+		if add_help == argparse.SUPPRESS:
+			# add -h but omit it from the help
+			self.add_argument(
+				"-h", "--help", action='help',
+				default=argparse.SUPPRESS,
+				help=argparse.SUPPRESS)
 
 	def add_subparsers(self, *args, **kwargs):
 		optional = kwargs.pop("optional", None)
 		if optional:
 			kwargs["action"] = self.OptionalSubCommands
 			if not "title" in kwargs:
-				kwargs["title"] = "optional subcommands"
+				kwargs["title"] = "Optional subcommands"
 		elif optional is False or "action" not in kwargs:
 			kwargs["action"] = self.MandatorySubCommands
 			if not "title" in kwargs:
-				kwargs["title"] = "possible subcommands"
+				kwargs["title"] = "Possible subcommands"
 
 		return super().add_subparsers(*args, **kwargs)
 
 	def add_some_of_group(self, **kwargs):
 		if "title" not in kwargs:
-			kwargs["title"] = "choose one or more from"
+			kwargs["title"] = "Choose one or more from"
 		group = self.add_argument_group(**kwargs)
 		self.some_of_groups.append(group)
 		return group
@@ -196,6 +295,7 @@ class ArgumentParserExtra(argparse.ArgumentParser):
 			if callable(alternative):
 				try: return alternative(value)
 				except ValueError: continue
+				except self.ArgumentTypeError: continue
 			elif isinstance(alternative, __class__.regexp_class):
 				try: return __class__.match_regexp(value,
 						alternative)
@@ -213,10 +313,15 @@ class ArgumentParserExtra(argparse.ArgumentParser):
 				candidates = [ alternative ]
 			else:
 				candidates.append(alternative)
+		if not value:
+			return None
 		return candidates
 
 	@classmethod
 	def multi_alternatives(self, alternatives):
+		if not alternatives:
+			return None, None
+
 		doc = [ ]
 		fixed = 0
 		for alternative in alternatives:
@@ -258,7 +363,11 @@ class ArgumentParserExtra(argparse.ArgumentParser):
 		if candidates is None:
 			candidates, is_plural = self.multi_alternatives(
 				alternatives)
-			if is_plural:
+			if candidates is None:
+				raise argparse.ArgumentTypeError(
+					"\"%s\": cannot accept any argument"
+						% value)
+			elif is_plural:
 				error = "\"%s\" should be either %s"
 			else:
 				error = "\"%s\" should be %s"
@@ -330,9 +439,14 @@ class ArgumentParserExtra(argparse.ArgumentParser):
 				candidates, is_plural = \
 					__class__.multi_alternatives(
 						alternatives)
-				if is_plural:
-					kwargs["help"] = \
-						"choose from " + candidates
+				if candidates is None:
+					kwargs["help"] = argparse.SUPPRESS
+				elif is_plural:
+					kwargs["help"] = "Choose from %s." \
+						% candidates
+				else:
+					kwargs["help"] = "Can only be %s." \
+						% candidates
 
 		if args[0][0] not in self.prefix_chars:
 			# positional argument.  if it's nargs is '*'
@@ -352,6 +466,12 @@ class ArgumentParserExtra(argparse.ArgumentParser):
 			super()._check_value(action, value)
 
 	def parse_known_args(self, args=None, namespace=None):
+		# move the positional argument group to the end of the help
+		if len(self._action_groups) > 1 \
+			and self._action_groups[0] is self._positionals:
+			self._action_groups.append(self._action_groups[0])
+			del self._action_groups[0]
+
 		self.subcommand_invoked = False
 		ns, argv = super().parse_known_args(args, namespace)
 
@@ -410,6 +530,10 @@ class ArgumentParserExtra(argparse.ArgumentParser):
 
 		del self.subcommands
 		return ns
+
+# a unique object to be used as default to indicate the absence of an argument
+UNSET = [ ]
+__all__.append("UNSET")
 
 # type-conversion functions
 anyint = re.compile("^0([0-9]+)$")
@@ -501,5 +625,198 @@ def hostname(host):
 
 	return host
 __all__.append("hostname")
+
+# the |. alternative is needed for error detection (m.groups()
+# would return None, None)
+hms_re = re.compile("(\d+(?:\.\d+)?)([wdhms])|.", re.IGNORECASE)
+def hms(arg):
+	if len(arg) == 0:
+		raise argparse.ArgumentTypeError("required argument missing")
+
+	sec = 0
+	for m in hms_re.finditer(arg):
+		n, d = m.groups()
+		if n is None:
+			raise argparse.ArgumentTypeError(
+				"expected something like `4w3d12h30m15s'")
+		n, d = float(n), d.lower()
+
+		if d == 'w':
+			sec += n * 7*24*60*60
+		elif d == 'd':
+			sec += n * 24*60*60
+		elif d == 'h':
+			sec += n * 60*60
+		elif d == 'm':
+			sec += n * 60
+		else:
+			sec += n
+	return int(round(sec))
+__all__.append("hms")
+
+# take a YYYY-MM-DDTHH:MM:SSZ-like timestamp and return its distance
+# from the epoch in milliseconds
+re_timestamp_tail = re.compile(r"((?:\.(\d+))?Z?)$")
+def timestamp(value):
+	import time
+
+	ms = 0
+	first, T, second = value.partition(' ' if ' ' in value else 'T')
+	if T:
+		dte, tme = first, second
+	elif '-' in first or '/' in first:
+		dte = first
+		tme = 0
+	else:	# @dte := today 00:00:00
+		tme = first
+		dte = int(time.time())
+		now = time.gmtime(dte)
+		dte -= now.tm_hour*60*60 + now.tm_min*60 + now.tm_sec
+
+	if isinstance(dte, str):
+		# convert @dte to number of seconds since epoch
+		if '/' in dte:
+			# add the year in order to be able to check
+			# the validity of the day of the month
+			dte = "%d/%s" % (time.gmtime().tm_year, dte)
+			fmt = "%Y/%m/%d"
+		else:
+			fmt = "%Y-%m-%d"
+
+		try:
+			dte = time.strptime(dte, fmt)
+		except ValueError:
+			raise ArgumentParserExtra.ArgumentTypeError(
+				"specify date as YYYY-MM-DD or MM/DD")
+		else:
+			dte = int(time.mktime(dte))
+
+	if isinstance(tme, str):
+		# strip the 'Z' suffix from @tme and extract the subseconds
+		m = re_timestamp_tail.search(tme)
+		if m.start(1) != m.end(1):
+			# there is a 'Z' suffix
+			tme = tme[:m.start(1)]
+			subsec = m.group(2)
+			if subsec: # subseconds were specified
+				# 0.500 -> 0.5
+				subsec = subsec.rstrip('0')
+			if subsec: # we have non-0 subseconds
+				if len(subsec) > 3:
+					# 0.1234s has sub-milliseconds
+					# precision, which we cannot handle
+					raise ArgumentParserExtra. \
+						ArgumentTypeError(
+							"cannot specify "
+							"time with more "
+							"than milliseconds "
+							"precision")
+
+				# 0.5s -> 500ms
+				ms = int(subsec)
+				for _ in range(0, len("999")-len(subsec)):
+					ms *= 10
+
+		# @tme := number of seconds since midnight
+		fmt = "%H:%M" if tme.count(':') == 1 else "%H:%M:%S"
+		try:
+			tme = time.strptime(tme, fmt)
+		except ValueError:
+			raise ArgumentParserExtra.ArgumentTypeError(
+				"specify time as HH:MM:SS or HH:MM")
+		else:
+			tme = tme.tm_hour*60*60 + tme.tm_min*60 + tme.tm_sec
+
+	return (dte + tme)*1000 + ms
+__all__.append("timestamp")
+
+if __name__ == '__main__':
+	WORDS = ("cohesion", "breed", "certified", "common",
+		"consuetudinary", "bestow", "be seated", "condensation",
+		"clustering", "briefed", "bromidic", "charge", "bevy",
+		"base", "circumscript", "be pregnant", "bewhiskered",
+		"company", "clamp", "candy", "condense", "chronic",
+		"bracket", "blueprinted", "crusty", "concretion", "band",
+		"clotting", "crystallize", "coherence", "coagulation",
+		"corny", "be knocked up", "battery", "cabal", "cocked",
+		"backing", "bend to", "constant", "cock", "cohort",
+		"banal", "bunch", "conformation", "borne", "cinch",
+		"compose", "cast", "category", "bullheaded", "crowd",
+		"compaction", "conglomerate", "branch", "constitution",
+		"bed", "chisel", "covey", "clinch", "conformable", "cramp",
+		"comportment", "create", "clock", "clabber", "congealment",
+		"belay", "cut to", "brigade", "crystallized", "brood",
+		"certain", "calloused", "carve", "confirm", "concrete",
+		"corps", "conventional", "caste", "congelation",
+		"bulldogged", "cemented", "charmed circle", "cake",
+		"chassis", "configuration", "coagulate", "cut-and-dried",
+		"bend", "complement", "bear", "cover", "backward motion",
+		"crusted", "console", "collection", "be-all", "cicatrize",
+		"charted", "broadcast", "current", "carry", "barb",
+		"build", "circumscribed", "contingent", "bulletheaded",
+		"be-all and " "end-all", "cohesiveness", "customary",
+		"curd", "calcified", "cut", "battalion", "congeal",
+		"callous", "contrived", "couple", "conglomeration",
+		"balky", "celestial navigation", "circle", "class",
+		"clump", "bump", "case-hardened", "closed circle",
+		"be gravid", "cluster", "beginning and end", "close",
+		"cutting", "coordinate", "build in", "bearing", "curdle",
+		"career", "conglobation", "clot", "bigoted", "copy",
+		"clasp", "concourse", "conduce", "confirmed", "clinging",
+		"cling", "cohere", "cleave", "block out", "burden with",
+		"booted and spurred", "balking", "calculated", "be with "
+		"child", "coterie", "consolidation", "cave in", "clique",
+		"course", "cave", "crew", "cell", "character", "blood",
+		"contribute", "bonded", "coached", "close up", "cling to",
+		"camarilla", "conflux", "bet", "backflowing", "crosscurrent",
+		"carry young", "bent", "cement", "commonplace", "bias",
+		"bearings", "confluence", "cadre", "block", "climbing",
+		"clan", "cabinet", "body")
+
+	def link_local_address(addr):
+		if ':' in addr:
+			for n in addr.split(':'):
+				if not 0 <= int(n, base=16) <= 255:
+					raise ValueError
+			return addr
+		else:
+			return ipv4_address(addr)
+	link_local_address.__name__ = "link-local address"
+
+	parser = ArgumentParserExtra()
+
+	parser.add_argument("--foo", type=integer)
+	parser.add_argument("--bar", type=unsigned)
+	parser.add_argument("--baz", type=float)
+	parser.add_argument("--bazz", action="on-off")
+	parser.add_argument("--buzz", action="boolean")
+	parser.add_argument("--buzi", multi=WORDS + (10, 20, 30))
+	parser.add_argument("--buza", multi=(unsigned, "forever"))
+	parser.add_argument("--biza", multi=(ip_address, unsigned), nargs=2)
+
+	sub = parser.add_subparsers(optional=False)
+	sub.add_parser("alpha").add_argument("--boo", type=ipv4_address)
+	sub.add_parser("gamma").add_argument("--poo", type=ip_address)
+	sub.add_parser("delta").add_argument("--too", type=ip_prefix)
+	sub.add_parser("sigma").add_argument("--moo", type=hostname)
+	sub.add_parser("summa").add_argument("--goo", type=link_local_address)
+
+	sub = sub.add_parser("epsilon", aliases=("kaksi", "katu"))
+	sub = sub.add_subparsers().add_parser("omega")
+	sub = sub.add_some_of_group()
+	sub.add_argument("--akarmi", metavar="FWMARK",
+		regexp="/(?:0x[0-9a-f]+|[0-9]+)"
+			"(?:/(?:0x[0-9a-f]+|[0-9]+))?/i")
+	sub.add_argument("--valami", type=sub.mkre("/[A-Z]+/"))
+	sub.add_argument("--barmi", type=re.compile("([AEIOU]+)"))
+	sub.add_argument("--semmi", regexp="/[^/]+/", action="append")
+	sub.add_argument("--nemtommi", action="store_true")
+	sub.add_argument("hi_hi_hi", nargs='*',
+		action="append", default=[["pina"]],
+		multi=(sub.mkre("/[01]+/"), unsigned, "haha"))
+
+	subcommands = [ ]
+	print(parser.parse_args(subcommands=subcommands))
+	print(subcommands)
 
 # End of arpextra.py
